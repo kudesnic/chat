4e2bb6d1d0ae29c466a8c49bcc83c7309452090e
testuser <andrii.derevianko@daxx.com>
2020-02-21 13:25:17 +0200

    dsdsdsds

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 sh get-docker.sh
 #composer create-project symfony/skeleton app
-sudo apt-get install curl \
-&& sudo curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose \
+sudo curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose \
 && sudo chmod +x /usr/local/bin/docker-compose \
 && docker-compose build --no-cache
